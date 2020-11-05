b975ba7923ef8eeeaccb732fdcee4d66c72ea472
Maksym Kuzmin <maksym.kuzmin.dev@gmail.com>
2020-11-05 14:22:45 +0200

    chore: initial commit

---
+++ src/components/commit-item/commit-item.tsx
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import React, { useEffect } from 'react';
+
+const CommitItem: React.FC = () => {
+  return <div>Hi, i`m a commit</div>;
+};
+
+export default CommitItem;
