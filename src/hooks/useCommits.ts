0790f5b45697deb000b9a7f9905f8c1687bdc9fc
Maksym Kuzmin <maksym.kuzmin.dev@gmail.com>
2020-11-05 15:41:22 +0200

    feat(hooks): add commits hook

---
+++ src/hooks/useCommits.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { CommitModel } from '../components/models/commit-model';
+
+import { API_URL } from '../constants/api-url';
+
+export const useCommits = () => {
+  const [data, setData] = useState<{ commits: CommitModel[] }>({ commits: [] });
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchCommits = async () => {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const { data } = await axios.get<CommitModel[]>(API_URL);
+        setData({ commits: data });
+      } catch (e) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchCommits();
+  }, []);
+  return { data, isError, isLoading };
+};
